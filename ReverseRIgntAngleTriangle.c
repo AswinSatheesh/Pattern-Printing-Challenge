c6618b3a98eb5a74d5b636f17ec3c64b3df5a0f3
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-11-01 23:03:34 +0530

    another method learned and added

--- ReverseRIgntAngleTriangle.c
+++ ReverseRIgntAngleTriangle.c
@@ -2,10 +2,21 @@
 #include<stdio.h>
 int main()
 {
-    int num=8;
-    for(int row =num; row >=1; row--)
+    int num=5;
+    // for(int row =num; row >=1; row--)
+    // {
+    //     for(int col = 1; col <=row; col++)
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // return 0;
+
+    for(int row =1; row <=num; row++)
     {
-        for(int col = 1; col <=row; col++)
+        for(int col =1; col <= num-row+1;col++)
         {
             printf("* ");
         }
