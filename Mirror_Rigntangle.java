12c90542053ccb31b65643d75711639be9b342d6
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-11-14 06:56:33 +0530

    Mirror Righttriangle completed

--- Mirror_Rigntangle.java
+++ Mirror_Rigntangle.java
@@ -2,23 +2,21 @@ import java.util.Scanner;
 public class Mirror_Rigntangle{
     public static void main(String[] args)
     {
- 	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter N : ");
-	int n=sc.nextInt();	 
-               System.out.print("Enter Symbol : ");
-	char c = sc.next().charAt(0);
-	for(int i=1;i<=n;i++)
-               {
-	 for(int j=0;j<n-i;j++)
-                       {
-                               System.out.print(" ");
-                       }
-	
-               for(int j=0;j<i;j++)
-                       {
-                               System.out.print(c);
-                       }
-                     System.out.println();
-               }             
+ 	System.out.println("Enter the no. of rows:\n");
+        Scanner sc = new Scanner(System.in);
+        int row  = sc.nextInt();
+        for(int i = 1;i <= row; i++)
+        {
+                for(int j=0; j < row-i; j++)
+                {
+                        System.out.print(" ");
+                        
+                }
+                for(int k =0; k < i; k++)
+                {
+                        System.out.print("*");
+                }
+                System.out.println();
+        }             
     }
 }
