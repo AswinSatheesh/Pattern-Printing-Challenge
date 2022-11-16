c3aa31e1e351da3808b0179bda2544b9544e3c33
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-11-16 07:14:07 +0530

    pascal's triangle completed and learned

---
+++ PascalTriangle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class PascalTriangle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int spaces =n,number=1;
+        for(int i = 0; i < n; i++)
+        {
+            for(int s=1;s<=spaces;s++)
+            {
+                System.out.print(" ");
+            }
+            number =1;
+            for(int j=0; j<=i; j++)
+            {
+                System.out.print(number + " ");
+                number = number * (i -j)/(j+1);  //formulla
+            }
+            spaces--;
+            System.out.println();
+        }
+
+    }
+}
